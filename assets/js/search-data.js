faaa0b80f526717e91ce41b6a5704ff4369a48a4
vyvas33 <113670456+vyvas33@users.noreply.github.com>
2025-05-16 07:53:09 +0000

    Deploying to gh-pages from @ vyvas33/vyvas33.github.io@a5e28a38e559233174b3d841baabe25458a51432 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,63 +11,38 @@ ninja.data = [{
     },
   },{id: "nav-portfolio",
           title: "Portfolio",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-resume",
+          title: "Resume",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/CV_Vyvaswath.pdf";
+            window.location.href = "/assets/pdf/Resume.pdf";
           },
-        },{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+        },{id: "projects-gesture-controlled-sma-actuated-robotic-hand",
+          title: 'Gesture Controlled SMA Actuated Robotic Hand',
+          description: "A robotic hand actuated by shape memory alloy springs and controlled using a gesture glove.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-an-articulated-robot-for-complex-and-confined-environments",
+          title: 'An Articulated Robot for Complex and Confined Environments',
+          description: "An inspection robot capable of in pipe and ground traversal.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-heat-transfer-characteristics-of-synthetic-jets",
+          title: 'Heat Transfer Characteristics of Synthetic Jets',
+          description: "An experimental study of elliptic synthetic jets with high aspect ratios",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-investigation-of-laser-soldering",
+          title: 'Investigation of Laser Soldering',
+          description: "Explored the use of lasers in soldering to improve efficiency and precision in the process.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/4_project-copy/";
             },},{
         id: 'social-email',
         title: 'email',
